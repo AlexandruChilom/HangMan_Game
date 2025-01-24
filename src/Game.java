00a68442f116d0a3c256419cdb945ff9e859d8c4
AlexandruChilom <alexchilom30@gmail.com>
2025-01-24 15:35:04 +0100

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,11 @@
+public class Game {
+    public static void main(String[] args) {
+        // Task1 : Afiseaza un mesaj de welcome : Welcome to HangMan Game!
+        // Task2 : Salveaza un cuvant intr-o variabila : Ex ariplane
+        // Task3 : Salveaaza si afiseaza atatea stelute cate trebuie
+        // Task3 : Afiseaza atatea stelute cate trebuie
+        // Task4 : Cere de la user o litera
+        // Task5 : Verifica daca litera introdusa exista in cuvant si inlocuieste steluta din array la indexul respectiv
+        // Task6 : Introdu un loop ( while ) care repeta de la task-ul 4
+    }
+}
